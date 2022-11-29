2670a0cff17b4baecbc7f24da0555aee2c523236
tejasugale9363 <119409270+tejasugale9363@users.noreply.github.com>
2022-11-29 16:43:42 +0530

    Add files via upload

---
+++ hungry.pyB3.py
@@ -0,0 +1,9 @@
+hungry=input("are you hungry")
+if hungry=="yes" or  hungry=="Y" or  hungry=="y":
+	print("eat sammosa")		
+	print("eat burger")
+	print("eat pizza")
+	print("eat franky")
+else
+	print("do hommework")
+													
